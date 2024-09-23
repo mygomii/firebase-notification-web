05ae7a3fc5ef5dce44de74f70a7114659799472f
mijeong.ko <mijeong.ko@hunet.co.kr>
2024-09-23 15:46:31 +0900

    * init commit

--- src/main/kotlin/com/mygomii/firebasenotificationweb/controller/TokenController.kt
+++ src/main/kotlin/com/mygomii/firebasenotificationweb/controller/TokenController.kt
@@ -1,5 +1,6 @@
 package com.mygomii.firebasenotificationweb.controller
 
+import com.mygomii.firebasenotificationweb.data.models.TokenReq
 import com.mygomii.firebasenotificationweb.data.models.TokenRes
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -8,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class TokenController {
     @PostMapping("/token")
-    fun insertToken(@RequestBody tokenRes: TokenRes): TokenRes {
-        println("token : ${tokenRes.token}")
-        val isSuccess = tokenRes.token?.isNotBlank() ?: false
-        return TokenRes(isSuccess = isSuccess, token = tokenRes.token)
+    fun insertToken(@RequestBody tokenReq: TokenReq): TokenRes {
+        println("token : ${tokenReq.token}")
+        val isSuccess = tokenReq.token.isNotBlank()
+        return TokenRes(isSuccess = isSuccess, token = tokenReq.token)
     }
 }
